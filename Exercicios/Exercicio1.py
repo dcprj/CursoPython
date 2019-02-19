394daf38653ca3f3645508b538209b23152cbee6
Dario Pinheiro <dpinheiro@gmail.com>
2019-02-18 22:37:30 -0300

    Exercicio1

---
+++ Exercicios/Exercicio1.py
@@ -0,0 +1,9 @@
+'''
+1. A imobiliária Imóbilis vende apenas terrenos retangulares. Faça um algoritmo para ler as
+dimensões de um terreno e depois exibir a área do terreno.
+'''
+
+lado1 = int(input("Digite o valor do lado 1 -> "))
+lado2 = int(input("Digite o valor do lado 2 -> "))
+area = lado1 * lado2
+print('A área do terreno é:', area, 'm²')
